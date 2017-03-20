a7f33d9d1a329f6e2e00e56331cb5bab2e555830
vagrant <vagrant@precise64.(none)>
2017-03-20 23:36:14 +0000

    Add user models and home + signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/home', to: 'pages#home'
+
+  get 'signup', to: 'pages#signup'
+
+  resources :users
+
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
