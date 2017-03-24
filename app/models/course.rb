767ec3c87b2fa9f67f7fc929c71538506b360a2d
vagrant <vagrant@precise64.(none)>
2017-03-24 03:01:24 +0000

    got courses-subjects association correct, reseeded database, needed to add enroll and improve search function

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,14 @@ class Course < ApplicationRecord
   has_and_belongs_to_many :subjects
 
   def self.search(parameters)
-    #Here until I understand more about the has_many stuff.
-    Course.where("name LIKE ?", parameters[:course_name])
+    if parameters[:course_name].empty? #User left the Name field blank
+      s = Subject.find(parameters[:subject_name]) #Do-able since subjects and courses are in a has_and_belongs_to_many relation
+      return s.courses
+    elsif Integer(parameters[:subject_name]) == 0 #User left the Subject field blank
+      Course.where("name LIKE ?", parameters[:course_name])
+    else #User filled in both fields
+      Course.where("name LIKE ?", parameters[:course_name]).merge(
+      Course.joins(:subjects).where("name LIKE ?", parameters[:subject_name]))
+    end
   end
 end
--- db/migrate/20170323052226_change_user_course_join_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserCourseJoinTable < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :courses_users, :enrollment
-  end
-end
